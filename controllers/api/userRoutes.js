c6a6e5e94b205a7bacc2c4f8cdf0b93b52e9c969
Cameron Houck <cameronhouck1@gmail.com>
2021-07-15 23:20:11 -0400

    adding api routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -15,7 +15,6 @@ router.post("/", async (req, res) => {
     res.status(400).json(err);
   }
 });
-
 router.post("/login", async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
