9c6f99ad86b159d0d833e14f2a4fc028a9b21ca9
Cameron Houck <cameronhouck1@gmail.com>
2021-07-20 12:12:41 -0400

    fixing soemthing

--- controllers/index.js
+++ controllers/index.js
@@ -2,13 +2,11 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
 const profileRoutes = require("./profileRoutes");
-const postRoutes = require("./postRoutes");
 
 
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
 router.use("/profile", profileRoutes);
-router.use("/posts", postRoutes);
 
 router.use((req, res) => {
   res.send("<h1>Wrong Route!</h1>");
